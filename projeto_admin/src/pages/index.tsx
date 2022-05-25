ee3ae414bef734cebeb4f012d7ec4d287ad0b590
anaves <anaves@gmail.com>
2022-05-25 10:21:29 -0300

    aula 24/05/2022 - estrutura do projeto final

---
+++ projeto_admin/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout titulo="Pagina inicial" subtitulo="Estamos construindo um template admin">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </Layout>
+  )
+}
+
+// return (
+//   <div className={`bg-gradient-to-r from-green-500 to-blue-500 h-screen`}>
+//     <h1>
+//       Pagina admin
+//     </h1>
+//   </div>
+// )
