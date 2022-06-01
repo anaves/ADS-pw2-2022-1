4f6bea8634b06e882074da437ef40b75458c23f5
anaves <anaves@gmail.com>
2022-05-31 22:25:07 -0300

    Aula 31-05-2022

--- projeto_admin/src/components/MenuLateral.tsx
+++ projeto_admin/src/components/MenuLateral.tsx
@@ -1,8 +1,17 @@
+import { iconeAjustes, iconeCasa } from "./icons/index";
+import Logo from "./Logo";
+import MenuItem from "./MenuItem";
 
 export default function MenuLateral(){
     return (
         <aside>
-            <h1>Menu</h1>
+            <div className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-300 h-20 w-20`}>
+                <Logo />
+            </div>
+            <ul>
+                <MenuItem url="/" texto="Inicio" icone={iconeCasa} />
+                <MenuItem url="ajustes" texto="Ajustes" icone={iconeAjustes} />
+            </ul>
         </aside>
     )
 }
---
+++ projeto_admin/src/pages/ajustes.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Ajustes(){
+    return (
+        <Layout titulo="Ajustes" subtitulo="Gerenciar configuracoes">
+            <h3>Conteudo da pagina ajustes</h3>
+        </Layout>
+    )
+}
